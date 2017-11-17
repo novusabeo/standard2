925d1e58e325e1b33c6ef0acffeb39cba5d8126f
novusabeo <33737555+novusabeo@users.noreply.github.com>
2017-11-16 23:10:11 -0700

    Create ListProgram.c

---
+++ ListProgram.c
@@ -0,0 +1,38 @@
+//Program to to find the sum of all the multiples of 3 or 5 below a number entered
+/*John Morrow
+November 16, 2017*/
+
+#include <stdio.h>
+
+int main()
+{
+	int uinput = 0; //User input
+	int i = 0; //Loop Variable
+	int f = 0; //Factor of 3 variable
+	int z = 0; //Loop Variable
+	int t = 0; //Factor of 5 variable
+	
+	do //Ensure that the entered variable is positive
+	{
+		printf("Please enter a positive integer to see what multiples of 3 and 5 it contains:\n");
+		scanf("%d", &uinput);
+	} while (uinput <= 0);
+
+	for (i = 0; i <= 333; i++) //Loop the multiplication of i by 3 if i is not a factor of 5
+	{
+		if (i % 5 == 0) {	//Avoids doubling up shared multiples of 3 and 5 in the list
+		}
+		else {
+			f = i * 3;
+			printf("%d\n", f); //List the resulting factors of 3 sequentially
+		}
+	}
+
+	for (z = 0; z <= 199; z++) //Loop the multiplication of i by 5
+	{
+		t = z * 5;
+		printf("%d\n", t);  //List the resulting factors of 5 sequentially
+	}
+	printf("\n");
+	return 0;
+}
